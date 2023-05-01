28a0e43b83c2b90701bf1902352c06d567f38ebc
Tico Thepsourinthone <tico.thepsourinthone@raymondjames.com>
2023-05-01 19:08:30 -0400

    completes 'very easy' python code challenge on 05/01/2023

---
+++ age_to_days.py
@@ -0,0 +1,9 @@
+# Create a function that takes in the age in years and returns the age in days.
+def calc_age(age):
+    age_in_years = age * 365
+    print(age_in_years)
+    return age_in_years
+
+
+calc_age(5)     # 1825
+calc_age(10)    # 3650
